9ba46f8ff68dd663a6edd8308dcbe1f6d48bbea0
wlstjd3398 <https://github.com/wlstjd3398/DB.git>
2021-06-07 16:22:49 +0900

    2021/06/07

---
+++ Crawling/1_request.py
@@ -0,0 +1,11 @@
+"""
+날짜 : 2021/06/07
+이름 : 김철학
+내용 : 파이썬 웹 Request 실습하기
+"""
+
+import requests as req
+
+# 네이버 페이지 요청
+response = req.get('http://naver.com')  # get 말고 post도 사용가능
+print(response.text)
---
+++ Crawling/2_parsing.py
@@ -0,0 +1,43 @@
+"""
+날짜 : 2021/06/07
+이름 : 김철학
+내용 : 파이썬 HTML 페이지 파싱 실습하기
+
+파싱(parsing)
+- 문서 해독을 의미
+- 마크업 문서(HTML, XML)에서 특정 태그의 데이터를 추출하는 처리과정
+"""
+# bs4 = beautifulsoup 패키지설치
+
+
+import requests as req
+
+from bs4 import BeautifulSoup as bs
+
+# 페이지 요청(네이버뉴스 헤드라인)
+response = req.get('https://news.naver.com/', headers={'User-Agent': 'Mozilla/5.0'})
+#print(response.text)
+
+# 페이지 파싱 dom = document object model
+dom = bs(response.text, 'html.parser')
+titles = dom.select('#today_main_news > div.hdline_news > ul > li > div.hdline_article_tit > a')
+#print(titles)
+
+# 파싱 데이터 출력(strip : 공백제거)
+for tit1 in titles:
+    print(tit1.next.strip())
+
+
+# 다음 뉴스 랭킹 1 ~ 10 출력하기
+
+
+resp = req.get('http://news.daum.net/ranking/popular')
+
+dom = bs(resp.text, 'html.parser')
+news_tits = dom.select('#mArticle > div.rank_news > ul.list_news2 > li:nth-child(n) > div.cont_thumb > strong > a')
+
+#print(news.tits)
+
+for i in range(10):
+    print(news_tits[i].text)
+
